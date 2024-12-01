56c8215282050a7d8758f5d2d5511bf7859d78ca
Olena <alchimeraa@gmail.com>
2024-12-01 19:41:25 +0200

    components

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import '../dist/assets/index-CB1iM0ge';
+import '../src/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
